6e813ff2619436455d0c679df7d62dfd9d0b4d07
Inder Pal <inder39811@gmail.com>
2023-09-05 10:14:38 +0530

    updates

--- socketHandlers/roomJoinHandler.js
+++ socketHandlers/roomJoinHandler.js
@@ -11,6 +11,17 @@ const roomJoinHandler = (socket, data) => {
   const roomDetails = serverStore.getActiveRooms(roomId);
   serverStore.joinActiveRoom(roomId, participantDetails);
 
+  // send info to user in room that they should prepare for incoming connection
+  roomDetails.forEach((roomDetail) => {
+    roomDetail.participants.forEach((participant) => {
+      if (participant.socketId !== participantDetails.socketId) {
+        socket.to(participant.socketId).emit("conn-prepare", {
+          connUserSocketId: participantDetails.socketId,
+        });
+      }
+    });
+  });
+
   roomsUpdates.updateRooms();
 };
 
